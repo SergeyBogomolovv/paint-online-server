26d0b10a4a6939daadd85d48f930939bd20c0e38
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-04-24 20:37:39 +0300

    fixed problem with rooms

--- src/drawing/drawing.gateway.ts
+++ src/drawing/drawing.gateway.ts
@@ -5,54 +5,48 @@ import {
   WebSocketServer,
   ConnectedSocket,
 } from '@nestjs/websockets';
-import { DrawingService } from './drawing.service';
 import { Server, Socket } from 'socket.io';
 import { Drawing } from './entities/drawing.entity';
 import { ConnectionEntity } from './entities/connection.entity';
-import { SaveDataEntity } from './entities/save-data-entity';
 
 @WebSocketGateway({ cors: { origin: '*' } })
 export class DrawingGateway {
   @WebSocketServer()
   server: Server;
-  constructor(private readonly drawingService: DrawingService) {}
+  constructor() {}
   @SubscribeMessage('connection')
   connect(
     @MessageBody() message: ConnectionEntity,
     @ConnectedSocket() client: Socket,
   ) {
     client.join(message.title);
-    client.broadcast.emit('connection', {
+    client.broadcast.to([...client.rooms]).emit('connection', {
       message: `Пользователь ${message.name} подключился`,
     });
   }
   @SubscribeMessage('draw')
   draw(@MessageBody() message: Drawing, @ConnectedSocket() client: Socket) {
     client.emit(message.type, message.figure);
-    client.broadcast.emit(message.type, message.figure);
+    client.broadcast.to([...client.rooms]).emit(message.type, message.figure);
   }
   @SubscribeMessage('finish')
   finish(@ConnectedSocket() client: Socket) {
-    client.broadcast.emit('finish');
+    client.broadcast.to([...client.rooms]).emit('finish');
     client.emit('finish');
   }
-  @SubscribeMessage('push')
-  push(@MessageBody() data: SaveDataEntity, @ConnectedSocket() client: Socket) {
-    client.broadcast.emit('push', data.data);
-    client.emit('push', data.data);
+  @SubscribeMessage('save')
+  push(@ConnectedSocket() client: Socket) {
+    client.broadcast.to([...client.rooms]).emit('save');
+    client.emit('save');
   }
   @SubscribeMessage('undo')
   undo(@ConnectedSocket() client: Socket) {
-    client.broadcast.emit('undo');
+    client.broadcast.to([...client.rooms]).emit('undo');
     client.emit('undo');
   }
   @SubscribeMessage('redo')
   redo(@ConnectedSocket() client: Socket) {
-    client.broadcast.emit('redo');
+    client.broadcast.to([...client.rooms]).emit('redo');
     client.emit('redo');
-  }
-  @SubscribeMessage('save')
-  save(@MessageBody() data: SaveDataEntity) {
-    this.drawingService.updateBoard(data.title, data.data);
   }
 }
