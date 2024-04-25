1944ee901263732ae2946230bec8e0f89a064421
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-04-25 15:53:02 +0300

    added connected users

--- src/drawing/drawing.gateway.ts
+++ src/drawing/drawing.gateway.ts
@@ -19,15 +19,29 @@ export class DrawingGateway {
     @MessageBody() message: ConnectionEntity,
     @ConnectedSocket() client: Socket,
   ) {
+    client.data.name = message.name;
     client.join(message.title);
+    const users = [];
+    this.server.sockets.adapter.rooms.get(message.title).forEach((id) => {
+      const socket = this.server.sockets.sockets.get(id);
+      users.push({ id: socket.id, name: socket.data.name });
+    });
+    client.on('disconnect', () => {
+      client.broadcast.to([...client.rooms]).emit('disconnection', {
+        name: message.name,
+        id: client.id,
+      });
+    });
+    client.emit('connection', { users, name: message.name });
     client.broadcast.to([...client.rooms]).emit('connection', {
-      message: `Пользователь ${message.name} подключился`,
+      users,
+      name: message.name,
     });
   }
   @SubscribeMessage('draw')
   draw(@MessageBody() message: Drawing, @ConnectedSocket() client: Socket) {
-    client.emit(message.type, message.figure);
     client.broadcast.to([...client.rooms]).emit(message.type, message.figure);
+    client.emit(message.type, message.figure);
   }
   @SubscribeMessage('finish')
   finish(@ConnectedSocket() client: Socket) {
